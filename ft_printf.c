be00a384b08f09d555bbc7deb7cf0c57560f64ff
Guilhermo Masid <guilhermomasid@gmail.com>
2022-06-17 13:38:45 -0300

    fix: code now works with multiple vars

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 17:18:50 by gmasid            #+#    #+#             */
-/*   Updated: 2022/06/09 13:23:48 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/06/17 13:35:59 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,23 @@ int	ft_putchar(int c)
 	return (1);
 }
 
-int	ft_parse_flag(va_list args, char match)
+int	ft_parse_flag(va_list *args, char match)
 {
 	int	result;
 
 	result = 0;
 	if (match == 'c')
-		result += ft_putchar(va_arg(args, int));
+		result += ft_putchar(va_arg(*args, int));
 	if (match == 'i' || match == 'd')
-		result += ft_print_int(va_arg(args, int));
+		result += ft_print_int(va_arg(*args, int));
 	if (match == 'u')
-		result += ft_print_uint(va_arg(args, unsigned int));
+		result += ft_print_uint(va_arg(*args, unsigned int));
 	if (match == 's')
-		result += ft_print_str(va_arg(args, char *));
+		result += ft_print_str(va_arg(*args, char *));
 	if (match == 'p')
-		result += ft_print_ptr(va_arg(args, unsigned long long));
+		result += ft_print_ptr(va_arg(*args, unsigned long long));
 	if (match == 'x' || match == 'X')
-		result += ft_print_hexa(va_arg(args, unsigned long long), match);
+		result += ft_print_hexa(va_arg(*args, unsigned long long), match);
 	if (match == '%')
 		result += ft_putchar('%');
 	return (result);
@@ -52,7 +52,7 @@ int	ft_printf(const char *s, ...)
 	while (s[i])
 	{
 		if (s[i] == '%')
-			result += ft_parse_flag(args, s[++i]);
+			result += ft_parse_flag(&args, s[++i]);
 		else
 			result += ft_putchar(s[i]);
 		i++;
